2131b33d4b91a102e6d137bc81d559830b2ff514
Mohammad <mysaltern@gmail.com>
2024-07-12 16:09:12 +1000

    multi call start fixed

--- modules/speech.js
+++ modules/speech.js
@@ -2,6 +2,7 @@ const { spawn } = require('child_process');
 const speech = require('@google-cloud/speech');
 const { handleVoiceCommand } = require('./voiceCommands'); // Import the command handler
 const GPT = require('../utils/chatGptApi'); // Import the TTS module
+
 class VoiceRecognition {
   constructor() {
     this.client = new speech.SpeechClient();
@@ -16,6 +17,8 @@ class VoiceRecognition {
     this.recognizeStream = null;
     this.convertedText = '';
     this.lastUpdateTime = Date.now();
+    this.commandQueue = []; // Queue to hold commands
+    this.isStoppingRecognition = false; // Add the flag here
   }
 
   startRecognition() {
@@ -33,7 +36,6 @@ class VoiceRecognition {
       .on('data', (data) => {
         if (data.results[0] && data.results[0].alternatives[0]) {
           const transcription = data.results[0].alternatives[0].transcript;
-          
 
           console.log(`Transcription: ${transcription}`);
           this.convertedText += transcription + ' ';
@@ -44,6 +46,8 @@ class VoiceRecognition {
       })
       .on('end', () => {
         console.log('Recognition stream ended.');
+        this.isStoppingRecognition = false; // Reset the flag when stream ends
+        this.processCommandQueue(); // Process any queued commands
       });
 
     const arecord = spawn('arecord', ['-f', 'S16_LE', '-r', '16000', '-c', '1', '-t', 'raw', '-q', '-']);
@@ -53,12 +57,20 @@ class VoiceRecognition {
   }
 
   stopRecognition() {
+    if (this.isStoppingRecognition) {
+      console.log('Recognition stop already in progress.');
+      return;
+    }
+
     if (this.recognizeStream) {
+      this.isStoppingRecognition = true; // Set the flag here
       this.recognizeStream.end();
       this.recognizeStream = null; // Ensure recognizeStream is reset
       console.log('Recognition stream stopped.');
     } else {
       console.log('No recognition stream to stop.');
+      this.isStoppingRecognition = false; // Reset the flag if no stream to stop
+      this.processCommandQueue(); // Process any queued commands
     }
   }
 
@@ -76,12 +88,19 @@ class VoiceRecognition {
     }
   }
 
+  processCommandQueue() {
+    while (this.commandQueue.length > 0) {
+      const command = this.commandQueue.shift();
+      command();
+    }
+  }
+
   checkAndUpdateLastUpdateTime() {
     setInterval(() => {
       if (Date.now() - this.lastUpdateTime > 3000 && this.convertedText.trim()) {
         console.log('Converted text unchanged for 3 seconds. Handling voice command...');
+        this.commandQueue.push(() => this.handleVoiceCommand()); // Queue the command
         this.stopRecognition(); // Use the existing instance to stop recognition
-        this.handleVoiceCommand(); // Use the existing instance to handle voice command
       }
     }, 1000);
   }
